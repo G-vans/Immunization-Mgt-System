6826577ac7d9db2661bbca746d983252213c5e21
Gvans <jevansstanley@gmail.com>
2024-02-16 17:50:38 +0300

    feat: create a user(medic) model with devise auth

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_16_144233) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_16_144800) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,6 +23,19 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_16_144233) do
     t.integer "weight"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "medics", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "username"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_medics_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_medics_on_reset_password_token", unique: true
   end
 
   create_table "vaccines", force: :cascade do |t|
