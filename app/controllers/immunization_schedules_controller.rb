0657cc447a35ff85aa67cf66067768f7b08a2520
Gvans <jevansstanley@gmail.com>
2024-03-24 12:39:49 +0300

    fix:fix signalwire token env variable

--- app/controllers/immunization_schedules_controller.rb
+++ app/controllers/immunization_schedules_controller.rb
@@ -74,23 +74,48 @@ end
   #twilio follow up call
 
   def follow_up_call
-    account_sid = ENV['TWILIO_ACCOUNT_SID']
-    auth_token = ENV['TWILIO_AUTH_TOKEN']
-    
-    @client = Twilio::REST::Client.new(account_sid, auth_token)
+    client = Signalwire::REST::Client.new ENV['SIGNALWIRE_PROJECT_KEY'], ENV['SIGNALWIRE_TOKEN'], signalwire_space_url: ENV['SIGNALWIRE_SPACE']
   
-    begin
-      call = @client.calls.create(
-        url: 'https://handler.twilio.com/twiml/EH807ccec3a6b78aae4071a71b43ff1f67',
-        to: '+254727538865',
-        from: '+15755793821'
-      )
+    call = client.calls.create(
+      url: ENV['APP_DOMAIN'],
+      to: ENV['TO_NUMBER'],
+      from: ENV['FROM_NUMBER']
+    )
+    flash[:notice] = 'Call to Mother Initiated....'
 
-      flash[:notice] = 'Call to Mother Initiated....'
-  
+    response = Signalwire::Sdk::VoiceResponse.new
+
+  if params['Digits'] || params['SpeechResult']
+    # We have input
+    if params['Digits'] == '1' || params['SpeechResult'].match?(/yes/)
+      response.say(message: "Thank you for confirming your missed schedule. Goodbye!")
+      response.hangup
+    elsif params['Digits'] == '2' || params['SpeechResult'].match?(/no/)
+      response.say(message: "We will reach out to you for further assistance. Goodbye!")
+      response.hangup
+    else
+      # We didn't understand
+      response.say(message: "Sorry, I could not understand you.")
+      response.redirect('/follow_up_missed_schedule?retry=1')
+    end
+  else
+    if params['retry'] != '1'
+      response.say(message: "Hello! This is a follow-up call from BabyShield.")
+      response.pause(length: 2)
+      response.say(message: "We noticed that you missed your baby's immunization schedule.")
+      response.pause(length: 2)
+      response.say(message: "Did you manage to get the immunization somewhere else? Press 1 for yes, or 2 for no and tell us the reason why.")
+    end
+
+    response.gather(action: '/handle-response', method: 'GET') do |gather|
+      gather.say(message: "If yes, please press 1. If not, please press 2 and tell us the reason why.")
+    end
+  end
+
+  render xml: response.to_s
       # Redirect to dashboard on success
       redirect_to dashboard_index_path, notice: 'Call to Mother Initiated....'
-    rescue Twilio::REST::TwilioError => e
+    rescue Signalwire::REST::Error => e
       # Handle Twilio API errors and display a flash alert
       redirect_to root_path, alert: "Error initiating call: #{e.message}"
     end
